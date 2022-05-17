55c52fd48a5db7a0c640886baf1c36e53db7e2ae
gsfzamai <gsfzamai@gmail.com>
2022-05-17 06:30:02 -0300

    ajustes redirecionamento

--- scripts/cadastro.php
+++ scripts/cadastro.php
@@ -3,13 +3,14 @@
     $nome = $_POST["nome"];
     $email = $_POST["email"];
     $senha = $_POST["senha"];
+    $baseUrl = $_SERVER["HTTP_HOST"];
     $senhaCriptografada = sha1($senha);
     $qtdLetras = strlen($senhaCriptografada);
 
     $query = "Insert into Usuarios(Nome, Email, Senha) Values('$nome', '$email', '$senhaCriptografada');";
     
     if($db->query($query)) {
-        header("Location: http://localhost/seminario03/index.php?s=1");
+        header("Location: http://$baseUrl/seminario03/index.php?s=1");
         echo "Registro Realizado com sucesso";
     }else {
         echo "Error: " . $query . "<br>" . $conn->error;
