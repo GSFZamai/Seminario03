924d1b86628df1b056d2712122db466381616e61
gsfzamai <gsfzamai@gmail.com>
2022-05-17 06:31:21 -0300

    ajustes redirecionamento

--- scripts/login.php
+++ scripts/login.php
@@ -2,6 +2,7 @@
     session_start();
     $db = require("./db.php");
     $login = $_POST["email"];
+    $baseUrl = $_SERVER["HTTP_HOST"];
     $password = sha1($_POST["password"]);
 
     $query = "SELECT Id, Nome FROM Usuarios WHERE Email = '$login' AND Senha = '$password';";
@@ -16,11 +17,11 @@
         $_SESSION["Nome"] = $response["Nome"];
         $_SESSION["Id"] = $response["Id"];
         $_SESSION["logged"] = true;
-        header("Location: http://localhost/seminario03/transacoes.php");
+        header("Location: http://$baseUrl/seminario03/transacoes.php");
     }else{
         echo "Error: " . $query . "<br>" . $conn->error;
         $_SESSION["logged"] = false;
-        header("Location: http://localhost/seminario03/index.php?login=1");
+        header("Location: http://$baseUrl/seminario03/index.php?login=1");
     }
 
     $db->close();
