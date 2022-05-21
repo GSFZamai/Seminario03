07f34ad025ef20ec2f773928edae0e1e8be33634
gsfzamai <gsfzamai@gmail.com>
2022-05-21 08:56:22 -0300

    adicioado bootstrap

--- scripts/db.php
+++ scripts/db.php
@@ -1,6 +1,8 @@
 <?php 
-    $db = new mysqli("localhost", "root", "", "financas");
-    
+    // $db = new mysqli("localhost", "root", "", "financas");
+ 
+    $db = new mysqli("sql10.freemysqlhosting.net","sql10492934","qRWHignh4P","sql10492934",3306);
+
     if($db) {
         return $db;
     }else {
--- transacoes.php
+++ transacoes.php
@@ -4,10 +4,7 @@ $nome = $_SESSION["Nome"];
 $id = $_SESSION["Id"];
 $db = require_once("./scripts/db.php");
 $serverHost = $_SERVER["HTTP_HOST"];
-$query = "
-SELECT 			
-(Select SUM(Valor) from Transacoes WHERE Tipo_Transacao = 1) as Entradas,
-(Select SUM(Valor) from Transacoes WHERE Tipo_Transacao = 2) as Saidas,
+$query = "SELECT (Select SUM(Valor) from Transacoes WHERE Tipo_Transacao = 1) as Entradas, (Select SUM(Valor) from Transacoes WHERE Tipo_Transacao = 2) as Saidas, 
 t.Id,
 t.Descricao,
 t.Data,
@@ -17,8 +14,7 @@ FROM
 Transacoes t
 LEFT JOIN Tipo_Transacao tt on(tt.Id = t.Tipo_Transacao)
 WHERE Id_Usuario = $id
-AND Exibe = 1;
-            ";
+AND Exibe = 1;";
 $transacoes = mysqli_fetch_all($db->query($query), 1);
 
 
@@ -33,8 +29,11 @@ $transacoes = mysqli_fetch_all($db->query($query), 1);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Transações de <?php echo $nome ?> </title>
 
+    <link rel="stylesheet" href="./libs/bootstrap/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.2/font/bootstrap-icons.css" />
+
     <script>
-       async function teste(id) {
+        async function teste(id) {
             var myInit = {
                 method: "POST",
                 body: JSON.stringify({
@@ -43,44 +42,63 @@ $transacoes = mysqli_fetch_all($db->query($query), 1);
             }
 
             fetch("./scripts/excluitransacao.php", myInit)
-            .then(res => {
-                if(res.status === 200) {
-                    document.location = "http://seminario03.herokuapp.com/transacoes.php"
-                }
-            })
-            .catch(rej => console.log(rej));
-            
+                .then(res => {
+                    if (res.status === 200) {
+                        document.location = "http://seminario03.herokuapp.com/transacoes.php"
+                    }
+                })
+                .catch(rej => console.log(rej));
+
         }
     </script>
 </head>
 
 <body>
-    <header>
-        <h1>Bem vindo(a)! <?php echo $nome ?></h1>
-    </header>
-    <form method="POST" action="./scripts/novatransacao.php">
-        <label>Descrição:</label>
-        <input required id="descricao" name="descricao" type="text" placeholder="Descrição da transação">
-        <label for="valor">Valor:</label>
-        <input required id="valor" name="valor"  type="number" step="0.01" placeholder="Valor da transação">
-        <select name="tipo" id="tipo" required>
-            <option value="1">Entrada</option>
-            <option value="2">Saída</option>
-        </select>
-        <input type="number" value=<?php echo $id ?> name="id" id="id" hidden="TRUE" />
-        <input type="submit" value="Cadastrar">
-    </form>
-    <?php
-    if (!$transacoes) {
-        echo "
-            <p>
-                Nenhuma Transação cadastrada!
-            </p>";
-    } else {
-        echo "
-        <table>
+    <div class="container">
+
+        <header>
+            <h1 class="text-center">Bem vindo(a)! <?php echo $nome ?></h1>
+        </header>
+        <form method="POST" action="./scripts/novatransacao.php">
+            <p>Insira uma nova transação:<p>
+            <div class="row">
+                <div class="col">
+                    <input class="form-control" required id="descricao" name="descricao" type="text" placeholder="Descrição da transação">
+                </div>
+                <div class="col">
+
+                    <div class="input-group">
+                        <span class="input-group-text">R$</span>
+                        <input class="form-control" required id="valor" name="valor" type="number" step="0.01" placeholder="Valor da transação">
+                    </div>
+
+                </div>
+                <div class="col">
+                    <select class="form-select" name="tipo" id="tipo" required>
+                        <option value="1">Entrada</option>
+                        <option value="2">Saída</option>
+                    </select>
+                </div>
+                <div class="col">
+                        <button class="btn btn-link p-0" type="submit"><i role="img" style="font-size: 25px" class="bi bi-plus-square-fill text-success"></i></i></button>
+
+                    </div>
+                <input type="number" value=<?php echo $id ?> name="id" id="id" hidden="TRUE" />
+            </div>
+        </form>
+        <?php
+        if (!$transacoes) {
+            echo "
+        <p>
+        Nenhuma Transação cadastrada!
+        </p>";
+        } else {
+            echo "
+        <p>Resumo de transações:<p>
+        <table class='table  table-striped'>
             <thead>
                 <tr>
+                    <td>Id</id>
                     <td>Descrição</td>
                     <td>Valor</td>
                     <td>Data</td>
@@ -89,34 +107,36 @@ $transacoes = mysqli_fetch_all($db->query($query), 1);
                 </tr>
             </thead>
             <tbody>";
-        foreach ($transacoes as $transacao) {
-            echo "
+            foreach ($transacoes as $transacao) {
+                echo "
                         <tr>
-                            <td>$transacao[Descricao]</td>
-                            <td>R$$transacao[Valor]</td>
-                            <td>$transacao[Data]</td>
-                            <td>$transacao[Tipo]</td>
-                            <td><input type='button' value='Excluir' onclick='teste($transacao[Id])'/></td>
+                        <th scrope='row'>$transacao[Id]</th>
+                        <td>$transacao[Descricao]</td>
+                        <td>R$$transacao[Valor]</td>
+                        <td>$transacao[Data]</td>
+                        <td>$transacao[Tipo]</td>
+                        <td><input type='button' class='btn btn-danger' value='Excluir' onclick='teste($transacao[Id])'/></td>
                         </tr>
-                    ";
+                        ";
+            }
+
+            echo "</tbody>";
+            $saldo = floatval($transacao['Entradas'] - $transacao['Saidas']);
+            echo
+            "<tfooter>
+                    <tr>
+                    <td>Saldo:</td>
+                    <td>R$ $saldo</td>
+                    </tr>
+                    </tfooter>";
         }
-
-        echo "</tbody>";
-        $saldo = floatval($transacao['Entradas'] - $transacao['Saidas']);
-        echo 
-        "<tfooter>
-            <tr>
-                <td>Total:</td>
-                <td>R$ $saldo</td>
-            </tr>
-        </tfooter>";
-    }
-    ?>
+        ?>
 
 
-    </table>
+        </table>
 
-
+    </div>
+    <script src="./libs/bootstrap/js/bootstrap.min.js"></script>
 </body>
 
 </html>
